28c2fc7cac7bae570f49df84772162c45988db10
Otibra <otienobrian718@gmail.com>
2024-05-01 09:01:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - data entry
+ * return 0(succes)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"};
+			puts("");
+
+			return (0);
+			}

cbd33e3efb03a12009c757e95e51e9b40fdd00f2
Otibra <otienobrian718@gmail.com>
2024-07-15 13:50:18 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-  * main - a program that prints a line using the printf function
-  * Return; 0(success)
-  */
+ * main - A program that prints a line using printf function
+ * Return: 0 when success
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

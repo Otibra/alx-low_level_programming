4928440aec71609b2904dfd8926590251915b4a0
Otibra <otienobrian718@gmail.com>
2024-07-18 10:11:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	printf("size of char: %lu bytes,sizeof(char)\n");
-	printf("size of int: %lu bytes, sizeof(int)\n");
-	printf("size of long int: %lu bytes, sizeof(long int)\n");
-	printf("size of long long int: %LU bytes, sizeof(long long int)\n");
-	pritf("size of float: %lu bytes, sizeof(float)\n");
+	printf("size of char: %lu byte(s); sizeof(char)");
+	printf("size of int: %lu byte(s); sizeof(int)");
+	printf("size of long int: %lu byte(s); sizeof(long int)");
+	printf("size of long long int: %LU byte(s); sizeof(long long int)");
+	pritf("size of float: %lu byte(s); sizeof(float)");
 
 	return (0);
 }

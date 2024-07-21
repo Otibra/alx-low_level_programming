8f033046c447c8e91fc7e8bb122bafc6ab8b29f6
Otibra <otienobrian718@gmail.com>
2024-07-21 12:45:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	long int b;
 	long long int d;
 	float f;
-
+	
 	printf("size of a char: %lu byte(s)\n ", (unsigned long)sizeof(c));
 	printf("size of an int: %lu byte(s)\n ", (unsigned long)sizeof(a));
 	printf("size of a long int: %lu byte(s)\n ", (unsigned long)sizeof(b));

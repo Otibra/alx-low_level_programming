d2d691772fc5ac26e56fcc6b0862f2c2221c6057
Otibra <otienobrian718@gmail.com>
2024-07-25 20:11:05 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	char lower, q, e;
+
+	q = 'q';
+	e = 'e';
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower != q && lower != e)
+		{
+			putchar(lower);
+		}
+	}
+	return (0);
+}

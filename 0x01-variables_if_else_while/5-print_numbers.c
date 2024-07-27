8186031ac4c729330e9eb3450f767e490b157ce0
Otibra <otienobrian718@gmail.com>
2024-07-27 09:13:01 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - a program that print
+ *
+ * Description: all single digit numbers of base 10 starting from 0
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d ='0'; d <= '10'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+}

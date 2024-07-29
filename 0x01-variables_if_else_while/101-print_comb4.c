08fe8053f20baba4f0f2d62e3736cb893479cd48
Otibra <otienobrian718@gmail.com>
2024-07-28 21:16:04 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,23 +12,22 @@ int main(void)
 {
 	int p, q, r;
 
-	for (p = 0; p <= 7; p++)
+	for (p = '0'; p < '7'; p++)
 	{
-		for (q = p + 0; q <= 8; q++)
+		for (q = p + 1; q < '8'; q++)
 		{
-			for (r = q + 1; r <= 9; r++)
+			for (r = q + 1; r < '9'; r++)
 			{
 				if ((p != q) != r)
 				{
-					putchar(p + '0');
-					putchar(q + '0');
-					putchar(r + '0');
+					putchar(p);
+					putchar(q);
+					putchar(r);
 
-					if (p == 7 && q == 8 && r == 9)
-					{
+					if (p == '7' && q == '8')
+						continue;
 						putchar(',');
 						putchar(' ');
-					}
 				}
 			}
 		}

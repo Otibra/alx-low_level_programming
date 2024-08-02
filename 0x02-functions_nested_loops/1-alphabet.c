690cacb2f8f1aef98e630f95dfef3c70ea74d80b
Otibra <otienobrian718@gmail.com>
2024-08-02 09:23:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabet
+ *
+ * Return: o when success
+ */
+
+void print_alphabet(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		_putchar(lower);
+	}
+}

1d7b8e93f593c02d5f008c6a0f144422452c872a
Otibra <otienobrian718@gmail.com>
2024-08-03 10:18:52 -0700

    3-islower.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - function that prints
+ * print_alphabet_x10(void) - function that prints 10 times the alphabet, in lowercase
  *
  * Return: void
  */
